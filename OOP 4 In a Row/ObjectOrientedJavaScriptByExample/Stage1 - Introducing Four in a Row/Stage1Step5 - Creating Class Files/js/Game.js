7360c2bb041315e754cb856b9407b4b53894814d
jlayher <john.e.layher@gmail.com>
2020-06-09 02:27:51 -0400

    Completed Up To: "Connecting With The DOM Solution"

--- OOP 4 In a Row/ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Game.js
+++ OOP 4 In a Row/ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Game.js
@@ -1,4 +1,25 @@
 class Game {
+  constructor(){
+    this.board = New Board();
+    this.players = this.createPlayers();  //same confusion as to the use of "this" that I had in the Player constructor
+    this.ready = false;
+  }
 
-  
+  /**
+   * Creates two player objects
+   * @return {Array}  An array of two Player objects.
+   */
+  createPlayers(){
+    const players = [new Player("Player 1", 1, "#e15258", true),
+                     new Player("Player 2", 2, "#e59a13")];
+    return players;
+  }
+
+  /**
+   * Gets game readu for play
+   */
+  startGame(){
+
+
+  }
 }
--- OOP 4 In a Row/ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
+++ OOP 4 In a Row/ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step5 - Creating Class Files/js/Player.js
@@ -1,4 +1,20 @@
 class Player {
-
-  
+  constructor(name, id, color, active = false){
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = this.createTokens(21);  //the use of "this.createTokens" confuses me
+  }
+  /**
+  * Creates token objects for player
+  * @param   {integer}   num - Number of token objects to be created
+  * @return  {array}     tokens - an array of new token objects
+  */
+  createTokens(num) {
+    const tokens = [];
+      tokens.push(token);
+    }
+    return tokens;
+  }
 }
